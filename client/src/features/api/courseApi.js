9f4b9002b3dbce0c3201dae7c3ed767a91caae8e
Vishal Aggarwal <007vishal.aggarwal@gmail.com>
2024-12-19 17:47:15 +0530

    Production changes 6 url changed

--- client/src/features/api/courseApi.js
+++ client/src/features/api/courseApi.js
@@ -1,7 +1,7 @@
 import EditCourse from "@/pages/admin/course/EditCourse";
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const COURSE_API = "http://localhost:8080/api/v1/course";
+const COURSE_API = "https://lms-gac5.onrender.com/api/v1/course";
 
 export const courseApi = createApi({
   reducerPath: "courseApi",
